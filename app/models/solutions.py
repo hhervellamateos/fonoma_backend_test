bdcfe3c31aaba8bf3eba43dc63ce8ddbd2067677
hhervellmateos <hhervellamateos@gmail.com>
2023-05-05 12:56:36 -0400

    validate quantity > 0

--- app/models/solutions.py
+++ app/models/solutions.py
@@ -14,6 +14,12 @@ class Order (BaseModel):
             raise ValueError(f'El precio {v} no puede ser negativo')
         return v
 
+    @validator('quantity')
+    def quantity_validate (cls, v):
+        if v <= 0:
+            raise ValueError("La cantidad debe ser mayor a 0")
+        return v
+
 class OrderList (BaseModel):
     orders: List[Order]
     criterion: Literal['completed', 'pending', 'canceled', 'all']
